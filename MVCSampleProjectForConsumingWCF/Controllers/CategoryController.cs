ae3f80d8e1f4ab6f5a9c3e824b55f6b367496b13
Jesus Fonseca <jefh15@hotmail.com>
2019-12-14 18:48:00 -0600

    Adding the VIEWS, CONTROLLER AND THE WCF REFERENCE
    Trabajamos en la misma solucion todos,

---
+++ MVCSampleProjectForConsumingWCF/Controllers/CategoryController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCSampleProjectForConsumingWCF.ServiceReference1;
+
+namespace MVCSampleProjectForConsumingWCF.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ServiceReference1.category category = new ServiceReference1.category();
+
+        // GET: Category
+
+        public ActionResult getCategories()
+        {
+            Service1Client o = new Service1Client();//this object get all methods in the WCF
+            List<category> li = o.getCategory().ToList();
+            ViewBag.List = li;//to get in view the list
+            return View();
+        }
+
+        //this display the view with the empty form
+        public ActionResult insertCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]//this using for insert elemement
+        public ActionResult insertCategory(category obj)
+        {
+            Service1Client o = new Service1Client();//this object get all methods in the WCF
+            o.InsertCategory(obj);
+            return View();
+        }
+
+        public ActionResult updateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]//this using for update elemement
+        public ActionResult updateCategory(category obj)
+        {
+            Service1Client o = new Service1Client();//this object get all methods in the WCF
+            o.UpdateCategory(obj);
+            return View();
+        }
+
+        public ActionResult deleteCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]//this using for update elemement
+        public ActionResult deleteCategory(int id)
+        {
+            Service1Client o = new Service1Client();//this object get all methods in the WCF
+            o.DeleteCategory(id);
+            return View();
+        }
+    }
+}
---
+++ MVCSampleProjectForConsumingWCF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCSampleProjectForConsumingWCF.MvcApplication" Language="C#" %>
